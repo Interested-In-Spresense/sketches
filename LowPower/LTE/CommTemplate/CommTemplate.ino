413e75b14a789b01309226696bc939fa718fed5a
Interested-In-Spresense <chun202309@gmail.com>
2026-05-31 11:43:11 +0900

    Add lowpower sample

---
+++ LowPower/LTE/CommTemplate/CommTemplate.ino
@@ -0,0 +1,194 @@
+/*
+ *  CommTemplate.ino - LTE Communication Template for Low-Power Sensing
+ *  Author Interested-In-Spresense
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <LTE.h>
+#include <ArduinoMqttClient.h>
+#include <LowPower.h>
+
+// APN name
+#define APP_LTE_APN "iijmio.jp" // replace your APN
+
+/* APN authentication settings
+ * Ignore these parameters when setting LTE_NET_AUTHTYPE_NONE.
+ */
+#define APP_LTE_USER_NAME "mio@iij"     // replace with your username
+#define APP_LTE_PASSWORD  "iij" // replace with your password
+
+// APN IP type
+#define APP_LTE_IP_TYPE (LTE_NET_IPTYPE_V4V6) // IP : IPv4v6
+// #define APP_LTE_IP_TYPE (LTE_NET_IPTYPE_V4) // IP : IPv4
+// #define APP_LTE_IP_TYPE (LTE_NET_IPTYPE_V6) // IP : IPv6
+
+// APN authentication type
+#define APP_LTE_AUTH_TYPE (LTE_NET_AUTHTYPE_CHAP) // Authentication : CHAP
+// #define APP_LTE_AUTH_TYPE (LTE_NET_AUTHTYPE_PAP) // Authentication : PAP
+// #define APP_LTE_AUTH_TYPE (LTE_NET_AUTHTYPE_NONE) // Authentication : NONE
+
+/* RAT to use
+ * Refer to the cellular carriers information
+ * to find out which RAT your SIM supports.
+ * The RAT set on the modem can be checked with LTEModemVerification::getRAT().
+ */
+
+#define APP_LTE_RAT (LTE_NET_RAT_CATM) // RAT : LTE-M (LTE Cat-M1)
+// #define APP_LTE_RAT (LTE_NET_RAT_NBIOT) // RAT : NB-IoT
+
+// MQTT broker
+#define BROKER_NAME "test.mosquitto.org"   // replace with your broker
+#define BROKER_PORT 1883                   // port 8883 is the default for MQTT over TLS.
+                                           // for this client, if required by the server.
+
+// MQTT topic
+#define MQTT_TOPIC "spresense/mqtt"        // replace with your topic
+
+// Deep sleep interval settings (adjust as needed)
+#define LOW_POWER_SLEEP_SEC 3600
+
+LTE lteAccess;
+LTEClient client;
+MqttClient mqttClient(client);
+
+char broker[] = BROKER_NAME;
+int port = BROKER_PORT;
+char topic[]  = MQTT_TOPIC;
+
+static void sleepForLowPower()
+{
+  Serial.print("Going to deep sleep for ");
+  Serial.print(LOW_POWER_SLEEP_SEC);
+  Serial.println(" sec.");
+  lteAccess.shutdown();
+  delay(100);
+  LowPower.deepSleep(LOW_POWER_SLEEP_SEC);
+}
+
+static void send()
+{
+  unsigned long currentTime = lteAccess.getTime();
+  String payload = "wakeup_post:" + String(currentTime);
+
+  // Publish one message after each wakeup.
+  Serial.print("Sending message to topic: ");
+  Serial.println(topic);
+  Serial.print("Publish: ");
+  Serial.println(payload);
+
+  mqttClient.beginMessage(topic);
+  mqttClient.print(payload);
+  mqttClient.endMessage();
+}
+
+void doAttach()
+{
+  while (true) {
+
+    /* Power on the modem and Enable the radio function. */
+
+    if (lteAccess.begin() != LTE_SEARCHING) {
+      Serial.println("Could not transition to LTE_SEARCHING.");
+      Serial.println("Please check the status of the LTE board.");
+      for (;;) {
+        sleep(1);
+      }
+    }
+
+    /* The connection process to the APN will start.
+     * If the synchronous parameter is false,
+     * the return value will be returned when the connection process is started.
+     */
+    if (lteAccess.attach(APP_LTE_RAT,
+                         APP_LTE_APN,
+                         APP_LTE_USER_NAME,
+                         APP_LTE_PASSWORD,
+                         APP_LTE_AUTH_TYPE,
+                         APP_LTE_IP_TYPE,
+                         false) == LTE_CONNECTING) {
+      Serial.println("Attempting to connect to network.");
+      break;
+    }
+
+    /* If the following logs occur frequently, one of the following might be a cause:
+     * - APN settings are incorrect
+     * - SIM is not inserted correctly
+     * - If you have specified LTE_NET_RAT_NBIOT for APP_LTE_RAT,
+     *   your LTE board may not support it.
+     */
+    Serial.println("An error has occurred. Shutdown and retry the network attach preparation process after 1 second.");
+    lteAccess.shutdown();
+    sleep(1);
+  }
+}
+
+void setup()
+{
+  // Open serial communications and wait for port to open
+  Serial.begin(115200);
+  while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("Starting .");
+
+  // Initialize LowPower API before using deep sleep.
+  LowPower.begin();
+
+  /* Connect LTE network */
+  doAttach();
+
+  // Wait for the modem to connect to the LTE network.
+  Serial.println("Waiting for successful attach.");
+  LTEModemStatus modemStatus = lteAccess.getStatus();
+
+  while (LTE_READY != modemStatus) {
+    if (LTE_ERROR == modemStatus) {
+
+      /* If the following logs occur frequently, one of the following might be a cause:
+       * - Reject from LTE network
+       */
+      Serial.println("An error has occurred. Shutdown and retry the network attach process after 1 second.");
+      lteAccess.shutdown();
+      sleep(1);
+      doAttach();
+    }
+    sleep(1);
+    modemStatus = lteAccess.getStatus();
+  }
+
+  Serial.println("attach succeeded.");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+    sleepForLowPower();
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+
+}
+
+void loop()
+{
+  send();
+
+  mqttClient.stop();
+  sleepForLowPower();
+}
